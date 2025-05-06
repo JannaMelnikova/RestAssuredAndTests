8aee4a45e9aaf4637b4dd8f1ac93c0dfd4c3fce2
Zhanna <jannet87@list.ru>
2025-05-06 17:28:37 +0300

    Initial commit

---
+++ src/test/java/ru/lesson/restassuredandtest/PostTest.java
@@ -0,0 +1,4 @@
+package ru.lesson.restassuredandtest;
+
+public class PostTest {
+}
---
+++ src/test/java/ru/lesson/restassuredandtest/RestAssuredAndTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lesson.restassuredandtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredAndTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
